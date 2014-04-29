4060aabfcb781f89516399afff3da9cd75288bdb
Weihong Guan <weihong.guan@gmail.com>
2014-04-29 22:27:59 +0800

    use flask-migrate for db migration and setup back file structure

--- config.py
+++ config.py
@@ -1,4 +1,12 @@
 # -*- coding: utf-8 -*-
 
+import os
+from app import app
+
+CSRF_ENABLED = True
+SECRET_KEY = 'you-will-never-guess'
+basedir = app.root_path
+SQLALCHEMY_DATABASE_URI = 'sqlite:///' + os.path.join(basedir, 'app.db')
+
 THERMOMETER_PATH = '/sys/bus/w1/devices/28-00000535631e/w1_slave'
 THERMOMETER_SCALE = 16
